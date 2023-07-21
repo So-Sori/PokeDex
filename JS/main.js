89f3672d07066332c9e16d296f37848b6d57bd7d
Soribel SB <soribelsantos0514@gmail.com>
2023-07-21 09:20:04 -0400

    Update loading dinamico

--- JS/main.js
+++ JS/main.js
@@ -141,18 +141,20 @@ botonesList.forEach(boton => boton.addEventListener("click", (event) => {
     let res = await fetch(URL + "?limit=898&offset=0");
     let data = await res.json();
     let template = document.createElement("div");
+    console.log("data",data);
 
     for (let i = 0; i <= data.results.length - 1; i++) {
         let res = await fetch(data.results[i].url);
         let json = await res.json();
+        console.log("json",json);
 
         const tipos = json.types.map(type => type.type.name);
         if (tipos.some(tipo => tipo.includes(botonId))) {
             template.innerHTML += showPokemon(json);
         }
+        listaPokemon.innerHTML =  template.innerHTML;
     }
     
-    listaPokemon.innerHTML =  template.innerHTML;
     loader.style.display = "none"
 }
     loaderType();
